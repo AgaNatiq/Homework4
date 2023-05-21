671508fdbd3cd1e70d06238718281d56eb6cdd83
AgaNatiq <natigmnu@code.edu.az>
2023-05-21 11:21:59 +0400

    homework4

---
+++ BankCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qaralama
+{
+    public class BankCard
+    {
+        public string BankName { get; set; }
+        public string CardHolder { get; set; }
+        public long CardNumber { get; set; }
+        public int CVV { get; set; }
+        public string ExpDate { get; set; }
+        public decimal CardBalance { get; set; }
+
+        public void IncreaseBalance(decimal amount)
+        {
+            if (amount < 0)
+                return;
+            CardBalance += amount;
+        }
+
+        public void DecreaseBalance(decimal amount)
+        {
+            if (amount < 0)
+                return;
+            if (amount > CardBalance);
+            CardBalance -= amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Qaralama
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankCard card= new BankCard();
+            card.BankName = "Kapital Bank";
+            card.CardHolder = "Natig";
+            card.CardNumber = 1342465596547415;
+            card.CVV = 321;
+            card.ExpDate = "09/23";
+            
+            card.IncreaseBalance(850m);
+            card.DecreaseBalance(325m);
+
+            Console.WriteLine(card.CardBalance);
+        }
+
+    }
+}
